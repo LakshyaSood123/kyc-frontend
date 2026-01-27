14b73da72fe936febf8504214bc8c8ffc351f591
LakshyaSood123 <newmaillakshya@gmail.com>
2026-01-27 15:18:46 +0530

    fix/frontend

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
 import { AppRouter } from "./router";
 import { ThemeProvider } from "../ui/ThemeContext";
-import { PortalAuthProvider } from "../lib/portalAuth";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <PortalAuthProvider>
-        <AppRouter />
-      </PortalAuthProvider>
+      <AppRouter />
     </ThemeProvider>
   );
 }
